532676d2a9ea9f97778ca555bac9dde513b032aa
wildanGhifari <wildghifari@gmail.com>
2021-07-04 21:57:04 +0700

    Create renderLeads() to render leads in myLeads[] into <ul>

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 let myLeads = []
 let inputEl = document.getElementById("input-el")
 let btnSave = document.getElementById("btn-save")
+let listEL = document.getElementById("list-el")
 
 btnSave.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    
+    renderLeads()
 })
+
+function renderLeads() {
+    let listItem
+
+    // Loop through myLeads
+    for (let i = 0; i < myLeads.length; i++) {
+        listItem = "<li>" + myLeads[i] + "</li>"
+    }
+
+    listEL.innerHTML += listItem
+}
