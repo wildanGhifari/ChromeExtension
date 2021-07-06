fcfbb1c6ad980eb557d1d9fe5ae91687a4b464a8
wildanGhifari <wildghifari@gmail.com>
2021-07-06 23:32:53 +0700

    Storing array to localStorage

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ let inputEl = document.getElementById("input-el")
 let btnSave = document.getElementById("btn-save")
 let listEL = document.getElementById("list-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
 btnSave.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
+
+    // Store myLeads to localstorage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
     renderLeads()
 })
 
