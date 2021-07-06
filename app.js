be60882aa466028ace0bf8a8b5a495f410f70605
wildanGhifari <wildghifari@gmail.com>
2021-07-07 01:54:25 +0700

    refactor renderLeads() to use parameter

--- app.js
+++ app.js
@@ -7,13 +7,30 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItem = ""
+
+    // Loop through leads
+    for (let i = 0; i < leads.length; i++) {
+        listItem += `
+            <li>
+                <a href='${leads[i]}' target='_blank'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+
+    listEL.innerHTML = listItem
 }
 
 btnDelete.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 btnSave.addEventListener("click", function(){
@@ -22,22 +39,5 @@ btnSave.addEventListener("click", function(){
 
     // Store myLeads to localstorage
     localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-    renderLeads()
+    render(myLeads)
 })
-
-function renderLeads() {
-    let listItem = ""
-
-    // Loop through myLeads
-    for (let i = 0; i < myLeads.length; i++) {
-        listItem += `
-            <li>
-                <a href='${myLeads[i]}' target='_blank'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-
-    listEL.innerHTML = listItem
-}
